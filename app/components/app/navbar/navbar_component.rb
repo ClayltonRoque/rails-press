67d47fd58e89cd71a013f77eb79979a0b0a678e0
claylton <claylton202@gmail.com>
2023-09-11 17:59:59 -0300

    criado o view-component navbar

---
+++ app/components/app/navbar/navbar_component.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+module App
+    module Navbar
+        class NavbarComponent < ViewComponent::Base
+            renders_many :items, 'App::Navbar::NavbarItemComponent'
+
+            def initialize(title)
+            @title = title
+            end
+        end
+    end
+end
