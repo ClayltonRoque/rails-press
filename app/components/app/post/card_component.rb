03791a32bce43e488a787407e3af6c392241086e
claylton <claylton202@gmail.com>
2023-09-12 17:44:16 -0300

    criado os componentes card e post

---
+++ app/components/app/post/card_component.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+module App
+    module Post
+        class CardComponent < ViewComponent::Base
+            def initialize(post:)
+                @post = post
+            end
+        end
+    end
+end
---
+++ app/components/app/post/post_component.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+module App
+    module Post
+        class PostComponent < ViewComponent::Base
+            def initialize(post:)
+                @post = post
+            end
+        end 
+    end
+end
